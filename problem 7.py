6935a9508142d99a7b1ad2b503f5b67cde2766d9
dwip44 <dwippatel9499@gmail.com>
2025-01-07 15:49:22 +0530

    Add files via upload

---
+++ problem 7.py
@@ -0,0 +1,8 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=120
+>>> b=a/60
+>>> print("the value of a in minutes is",a)
+the value of a in minutes is 120
+>>> print("the value of a in hours is",b)
+the value of a in hours is 2.0
---
+++ program 6.py
@@ -0,0 +1,8 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=2
+>>> print("the value of a is 2 hours")
+the value of a is 2 hours
+>>> b=a*60
+>>> print("the value of a in minutes is",b)
+the value of a in minutes is 12
