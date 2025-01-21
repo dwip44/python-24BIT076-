800dd4ea97ddda1c5ad3eb88b3fe2084d9874a24
dwip44 <dwippatel9499@gmail.com>
2025-01-21 15:36:04 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,8 @@
+a=10
+b=23
+c=67
+largest = max(a,b,c)
+smallest = min(a,b,c)
+print("the largest number is ", largest)
+print("the smallest number is ", smallest)
+
---
+++ 7.py
@@ -0,0 +1,9 @@
+year=int(input("enter the year"))
+if year%4==0:
+    print("the year is a leap year")
+elif year%100!=0:
+        print("the year is not a leap year")
+elif year%400==0:
+            print("the year is a leap year")
+else:
+                print(" the yar is not a leap year")
